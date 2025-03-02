f7dc37ae077460f64632e9e4386c95a9ce97d82d
admin <mametkadyrov.y@gmail.com>
2025-03-02 11:18:01 +0300

    qualifier

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,21 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
-    private Dog dog;
+    @Qualifier("dog")
+    private Animal animal;
     @Autowired
     private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(dog.toString());
+        System.out.println(animal.toString());
         System.out.println("At:");
         System.out.println(timer.getTime());
         System.out.println("________________________");
